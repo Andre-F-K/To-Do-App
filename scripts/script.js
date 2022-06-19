33a73b74d173ce27f4633186797d290b5cfc6dd3
Andre-F-K <98839949+Andre-F-K@users.noreply.github.com>
2022-06-19 14:32:24 +0200

    Commenting
    finished commenting
    final css
    need to do local storage!

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,10 @@ const userInputForm = document.getElementById('userInputForm');
  const listElement = document.querySelector('#tasks')
 
  userInputForm.addEventListener('submit', (e) =>{
+    //preventing form from refreshing
     e.preventDefault()
+    
+    //form validation (check if inputs are not empty)
     let x = []
     const dueDate = document.getElementById('dueDateInput').value
     const newTask = document.getElementById('newTaskInput').value
@@ -18,49 +21,59 @@ const userInputForm = document.getElementById('userInputForm');
         return
     }
 
+    //creating task dive to add individual tasks to 
     const taskElement = document.createElement('div');
     taskElement.classList.add('task');
 
+    //creating content element to input all of the content for an individual task
     const taskContentElem = document.createElement('div');
     taskContentElem.classList.add('content');
 
+    //appending the above content task to task element
     taskElement.appendChild(taskContentElem);
 
+    //creating the task description element to contain the task that is received from 'newTask Form'
     const taskInputEle = document.createElement('input');
     taskInputEle.classList.add('text');
     taskInputEle.type = 'text';
     taskInputEle.value = newTask;
     taskInputEle.setAttribute('readonly' , 'readonly')
 
+    //creating the due date element to contain the due date that is received from 'newTask Form'
     const taskDateEle = document.createElement('input')
     taskDateEle.classList.add('text')
     taskDateEle.type = 'date'
     taskDateEle.value = dueDate
     taskDateEle.setAttribute('readonly' , 'readonly')
 
+    //appending the task and dueDate to content div 
     taskContentElem.appendChild(taskInputEle)
     taskContentElem.appendChild(taskDateEle)
 
+    //creating actions 'edit' & 'delete' for task elements 
     const taskActionsEle = document.createElement('div')
     taskActionsEle.classList.add('actions')
 
-
+    //making the buttons for the delete and edit 
     const taskEditEle = document.createElement('button')
     taskEditEle.classList.add('edit')
     taskEditEle.innerHTML = 'Edit'
 
+    //making the buttons for the delete and edit 
     const taskDelEle = document.createElement('button')
     taskDelEle.classList.add('delete')
     taskDelEle.innerHTML = 'Delete'
 
+    //appending the buttons to the actions element created above
     taskActionsEle.appendChild(taskEditEle)
     taskActionsEle.appendChild(taskDelEle)
 
+    //appending actions element to task element  
     taskElement.appendChild(taskActionsEle)
 
     listElement.appendChild(taskElement)
 
-
+    //adding the functionality to the 'edit' and 'delete' buttons 
     taskEditEle.addEventListener('click', () => {
        if(taskEditEle.innerText === 'Edit'){
         taskDateEle.removeAttribute('readonly')
@@ -79,7 +92,7 @@ const userInputForm = document.getElementById('userInputForm');
     })
 
 
-
+    //convering user inputs to an object
     class TasksObj{
         constructor (task, dueDate){
             this.task = task
