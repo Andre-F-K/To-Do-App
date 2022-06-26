c23cf028d160049cf13e925687ed7bda033cfbc6
Andre-F-K <98839949+Andre-F-K@users.noreply.github.com>
2022-06-26 19:42:30 +0200

    Updated Script to new scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,9 @@
-<<<<<<< Updated upstream
+
 window.addEventListener('load', () => {
 	todos = JSON.parse(localStorage.getItem('todos')) || [];
 	const nameInput = document.querySelector('#name');
 	const newTodoForm = document.querySelector('#new-todo-form');
-=======
-const userInputForm = document.getElementById('userInputForm');
- let errorElement = document.getElementById('error')
- const listElement = document.querySelector('#tasks')
 
- 
-
- userInputForm.addEventListener('submit', (e) =>{
-    //preventing form from refreshing
-    e.preventDefault()
-    
-    //form validation (check if inputs are not empty)
-    let x = []
-    const dueDate = document.getElementById('dueDateInput').value
-    const newTask = document.getElementById('newTaskInput').value
-    if(newTask === '' || newTask == null || dueDate === '' ){
-        console.log(newTask)
-         x.push('Inputs cannot be empty')
-         
-    }
->>>>>>> Stashed changes
 
 	const username = localStorage.getItem('username') || '';
 
@@ -37,8 +17,6 @@ const userInputForm = document.getElementById('userInputForm');
 		e.preventDefault();
 
        
-
-<<<<<<< Updated upstream
 
 		const todo = {
 			content: e.target.elements.content.value,
@@ -150,101 +128,4 @@ function formValidate(){
         
     }
 }
-=======
-    //creating task dive to add individual tasks to 
-    const taskElement = document.createElement('div');
-    taskElement.classList.add('task');
 
-    //creating content element to input all of the content for an individual task
-    const taskContentElem = document.createElement('div');
-    taskContentElem.classList.add('content');
-
-    //appending the above content task to task element
-    taskElement.appendChild(taskContentElem);
-
-    //creating the task description element to contain the task that is received from 'newTask Form'
-    const taskInputEle = document.createElement('input');
-    taskInputEle.classList.add('text');
-    taskInputEle.type = 'text';
-    taskInputEle.value = newTask;
-    taskInputEle.setAttribute('readonly' , 'readonly')
-
-    //creating the due date element to contain the due date that is received from 'newTask Form'
-    const taskDateEle = document.createElement('input')
-    taskDateEle.classList.add('text')
-    taskDateEle.type = 'date'
-    taskDateEle.value = dueDate
-    taskDateEle.setAttribute('readonly' , 'readonly')
-
-    //appending the task and dueDate to content div 
-    taskContentElem.appendChild(taskInputEle)
-    taskContentElem.appendChild(taskDateEle)
-
-    //creating actions 'edit' & 'delete' for task elements 
-    const taskActionsEle = document.createElement('div')
-    taskActionsEle.classList.add('actions')
-
-    //making the buttons for the delete and edit 
-    const taskEditEle = document.createElement('button')
-    taskEditEle.classList.add('edit')
-    taskEditEle.innerHTML = 'Edit'
-
-    //making the buttons for the delete and edit 
-    const taskDelEle = document.createElement('button')
-    taskDelEle.classList.add('delete')
-    taskDelEle.innerHTML = 'Delete'
-
-    //appending the buttons to the actions element created above
-    taskActionsEle.appendChild(taskEditEle)
-    taskActionsEle.appendChild(taskDelEle)
-
-    //appending actions element to task element  
-    taskElement.appendChild(taskActionsEle)
-
-    listElement.appendChild(taskElement)
-
-    //adding the functionality to the 'edit' and 'delete' buttons 
-    taskEditEle.addEventListener('click', () => {
-       if(taskEditEle.innerText === 'Edit'){
-        taskDateEle.removeAttribute('readonly')
-        taskInputEle.removeAttribute('readonly')
-       taskInputEle.focus() 
-       taskEditEle.innerText = 'Save'
-       }else{
-        taskDateEle.setAttribute('readonly', 'readonly')
-        taskInputEle.setAttribute('readonly', 'readonly')
-        taskEditEle.innerText = 'Edit'
-       }
-    })
-
-    taskDelEle.addEventListener('click', () => {
-     listElement.removeChild(taskElement)   
-    })
-    
-    
-
-    
-    
-})
-
-let taskDatabase = []
-
-//adding onclick to button taking inputs and adding them to an object and then pushes them into array
-
-newTaskSubmitButton.addEventListener('click', (e) => {
-     let taskData = {
-        task : document.getElementById('newTaskInput').value,
-        dueDate : document.getElementById('dueDateInput').value
-        
-    }
-
-    taskDatabase.push(taskData)
-    
-    console.log(taskDatabase)
-    
-})
-
-let taskDatabaseJSON = JSON.push(taskDatabase)
-
-
->>>>>>> Stashed changes
